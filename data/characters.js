36a739ce84411460cc45db4029ec4527ebbd3325
gummygyaru <vponpobusiness@gmail.com>
2025-10-15 10:16:52 -0700

    Update characters.js

--- data/characters.js
+++ data/characters.js
@@ -39,7 +39,247 @@ window.FAENOIR_CHARACTERS = [
     raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
     winner: "Hibycus" // ✅ manually set winner
   },
-  // ... add objects for day 16–31 in the same format
+  {
+    day: 16,
+    title: "Dog Days",
+    mlNumber: "ML-021",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "RatTheLoser",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day16.png",
+    silhouette: "silhouettes/day16.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 17,
+    title: "Haunted Roaches",
+    mlNumber: "ML-015",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Paora",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day17.png",
+    silhouette: "silhouettes/day17.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 18,
+    title: "Hippity Hoppity",
+    mlNumber: "ML-017",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "lopilofficial",
+    species: "Bloomyte",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day18.png",
+    silhouette: "silhouettes/day18.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 19,
+    title: "Goopy Trouble",
+    mlNumber: "ML-019",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "RatTheLoser",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day19.png",
+    silhouette: "silhouettes/day19.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 20,
+    title: "Koala Doll",
+    mlNumber: "ML-018",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Shimura",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day20.png",
+    silhouette: "silhouettes/day20.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 21,
+    title: "Clownin Around",
+    mlNumber: "ML-019",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "That-Doe",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day21.png",
+    silhouette: "silhouettes/day21.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 22,
+    title: "Zombie Silly",
+    mlNumber: "ML-020",
+    toyhouse: "https://toyhou.se/35646509.raffle",
+    designer: "kaijukandy",
+    species: "Bloomyte",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day22.png",
+    silhouette: "silhouettes/day22.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 23,
+    title: "Wicked Nurse",
+    mlNumber: "ML-021",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "RatTheLoser",
+    species: "Bloomyte",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day23.png",
+    silhouette: "silhouettes/day23.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 24,
+    title: "T E E T H",
+    mlNumber: "ML-022",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Captin_Bean",
+    species: "Bloomyte",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day24.png",
+    silhouette: "silhouettes/day24.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 25,
+    title: "Butts and Bolts",
+    mlNumber: "ML-023",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Captin_Bean",
+    species: "Bloomyte",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day25.png",
+    silhouette: "silhouettes/day25.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 26,
+    title: "im horny",
+    mlNumber: "ML-024",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Captin_Bean",
+    species: "Bloomyte",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day26.png",
+    silhouette: "silhouettes/day26.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 27,
+    title: "Spooky Scary Skeletons",
+    mlNumber: "ML-025",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Captin_Bean",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day27.png",
+    silhouette: "silhouettes/day27.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 28,
+    title: "Graveyard Groove",
+    mlNumber: "ML-026",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Shimura",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day28.png",
+    silhouette: "silhouettes/day28.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 29,
+    title: "Phantom Parade",
+    mlNumber: "ML-027",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Captin_Bean",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day29.png",
+    silhouette: "silhouettes/day29.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 30,
+    title: "Candlelight Curse",
+    mlNumber: "ML-028",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Shimura",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day30.png",
+    silhouette: "silhouettes/day30.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  },
+  {
+    day: 31,
+    title: "Final Fright",
+    mlNumber: "ML-029",
+    toyhouse: "https://toyhou.se/YYYYY",
+    designer: "Captin_Bean",
+    species: "Delerian",
+    traits: [
+      "Check their Masterlist!",
+    ],
+    image: "images/day31.png",
+    silhouette: "silhouettes/day31.png",
+    raffleWebhook: "https://script.google.com/macros/s/AKfycbwR6iLtbrPPjfqPFO3MgqPH-IYSEjGQ189T5kQo6K5vQI-Tj7FvjAhvgjeby2NQNyzz/exec",
+    winner: "ToBeAnnounced"
+  }
+
 ];
 
 // Helper: map day => data
